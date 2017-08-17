a026a0fef514f947c071f5d6ba23862734b18b4f
proller <proller@github.com>
2017-08-17 18:58:52 +0300

    Auto version update to [54278]

--- dbms/cmake/version.cmake
+++ dbms/cmake/version.cmake
@@ -1,6 +1,6 @@
 # This strings autochanged from release_lib.sh:
-set(VERSION_DESCRIBE v1.1.54276-testing)
-set(VERSION_REVISION 54277)
+set(VERSION_DESCRIBE v1.1.54278-testing)
+set(VERSION_REVISION 54278)
 # end of autochange
 
 set (VERSION_MAJOR 1)
